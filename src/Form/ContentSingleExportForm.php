cd98a6b2b0c9be9b4f0cb5555892e157899f232b
Blanca.Esqueda <blanca.esqueda@opin.ca>
2017-06-16 08:37:15 -0400

    Update to use the centrlize export functionality

--- src/Form/ContentSingleExportForm.php
+++ src/Form/ContentSingleExportForm.php
@@ -173,122 +173,13 @@ class ContentSingleExportForm extends FormBase {
     $entity_bundle = $form_state->getValue('content_name');
     $entity_id = $form_state->getValue('content_entity');
 
-    //Validate that it is a Content Entity
-    $instances = $this->entityTypeManager->getDefinitions();
-    if(!( isset($instances[$entity_type]) && $instances[$entity_type] instanceof ContentEntityType ) ){
-      $output = t('Entity type does not exist or it is not a content instance.');
-    }else{
-      // Get Entity Fields.
-      $fields = array_filter(
-        $this->entityFieldManager->getFieldDefinitions($entity_type, $entity_bundle), function ($field_definition) {
-          return $field_definition;
-        }
-      );
+    // DB entity to YAML
+    module_load_include('inc', 'content_sync', 'content_sync.batch');
+    $entity = _content_sync_db_to_entity($entity_type,$entity_bundle,$entity_id);
+    $output = Yaml::encode($entity);
+    $name = $entity_type . "." . $entity_bundle . "." . $entity['values']['uuid'][0]['value'];
 
-      // Initialize array of elements to export.
-      $entity_elements = [];
-      foreach($fields as $fieldID => $field){
-        $entity_elements[$field->getName()] = $field->getName();
-      }
-
-      // Get Entity Properties - to know the id and bundle fields.
-      $properties = $this->entityTypeManager->getDefinitions()[$entity_type]->getKeys();
-      
-      // Get data to fill the yaml.
-      $entity_data = $this->entityTypeManager->getStorage($entity_type)->load($entity_id);
-      $entity = [];
-      $entity['entity_type'] = $entity_type;
-      $entity['bundle'] = $entity_bundle;
-      //Include Site UUID
-      $site_config = \Drupal::config('system.site');
-      $site_uuid_source = $site_config->get('uuid');
-      $entity['site_uuid'] = $site_uuid_source;
-
-      // Remove property ID as we are gonna use UUID to avoid conflicts.
-      unset($entity_elements[$properties['id']]);
-      // Remove bundle as it is defined already
-      unset($entity_elements[$properties['bundle']]);
-      // Remove vid to avoid conflicts w/versions
-      unset($entity_elements['vid']);
-      // Filter array
-      $entity_elements = array_filter($entity_elements);
-
-
-
-      //Get entity values
-      foreach ($entity_elements as $elementID => $element){
-        //Include parent UUID if it exist
-        if ($element == 'parent') {
-          $parent = $this->entityTypeManager->getStorage($entity_type)->loadParents($entity_id);
-          $parent = reset($parent);
-          if ( !empty($parent) ) {
-            $entity['values'][0][$element] = $parent->uuid();
-          }
-        }else {
-          $entity['values'][0][$element] = $entity_data->get($element)
-            ->getValue();
-        }
-        //Check if it is an entity reference and use UUID instead of target id.
-        $element_type = $entity_data->get($element)->getFieldDefinition()->getType();
-        if ( $element_type == "entity_reference" ||
-          $element_type == "image" ||
-          $element_type == "file" ) {
-          if ( $entity_data->get($element)->entity ) {
-            $reference_type = $entity_data->get($element)->entity->getEntityType()->id();
-            //Loop all the values
-            foreach ($entity_data->get($element)->getValue() as $er_key => $er_val) {
-              $entity['values'][0][$element][$er_key]['target_id'] = $this->entityTypeManager->getStorage($reference_type)->load($er_val['target_id'])->uuid();
-            }
-          }
-        }
-      }
-      // TODO - Check the path value.
-      // Exception to get the path as it can not be retrieved as regular value.
-      // Not set for image because gives an error.
-      //$current_path = \Drupal::service('path.current')->getPath();
-      if($entity_type != "file") {
-        $entity['values'][0]['path'] = "/" . $entity_data->toUrl()->getInternalPath();
-      }
-
-      // Include Translations
-      $lang_default = $entity['values'][0]['langcode'][0]['value'];
-      // Remove translations if they are in the import data the they would be inserted.
-      foreach ($entity_data->getTranslationLanguages() as $langcode => $language) {
-        $c = 0;
-        if($entity_data->hasTranslation($langcode)) {
-          $entity_data_translation = $entity_data->getTranslation($langcode);
-          // Verify that it is not the default langcode.
-          if ($langcode != $lang_default){
-            foreach ($entity_elements as $elementID => $element){
-              // Only translatable elements for translations
-              if ($fields[$elementID]->isTranslatable() == TRUE){
-                $entity['values'][0]['translations'][$c][$element] =  $entity_data_translation->get($element)->getValue();
-                //Check if it is an entity reference and use UUID instead of target id.
-                $element_type = $entity_data_translation->get($element)
-                  ->getFieldDefinition()
-                  ->getType();
-                if ( $element_type == "entity_reference" ||
-                  $element_type == "image" ||
-                  $element_type == "file" ) {
-                  if ( $entity_data_translation->get($element)->entity ) {
-                    $reference_type = $entity_data_translation->get($element)->entity->getEntityType()->id();
-                    //Loop all the values
-                    foreach ($entity_data_translation->get($element)->getValue() as $er_key => $er_val) {
-                      $entity['values'][0]['translations'][$c][$element][$er_key]['target_id'] = $this->entityTypeManager->getStorage($reference_type)->load($er_val['target_id'])->uuid();
-                    }
-                  }
-                }
-              } 
-            }
-            //$entity['translations'][$c]['path'] = $entity_data_translation->toUrl()->getInternalPath();
-            $c++;
-          }
-        }
-      }
-      $output = Yaml::encode($entity);
-      $name = $entity_type . "." . $entity_bundle . "." . $entity['values']['uuid'][0]['value'];
-    }
-
+    // Return form values
     $form['export']['#value'] = $output;
     $form['export']['#description'] = $this->t('Filename: %name', ['%name' => $name . '.yml']);
     return $form['export'];
