6072e531d9394ad45254c5de8b08015f5774801f
yogeshchaugule <yogeshchaugule@724666.no-reply.drupal.org>
2017-11-18 00:32:15 +0800

    Update Parents decorator to handle not only taxonomy parents, but open the option to menues and maybe other entities - Issue #2899099 by yogeshchaugule8: Unexpected error when changing order of menu items

--- src/Plugin/SyncNormalizerDecorator/Parents.php
+++ src/Plugin/SyncNormalizerDecorator/Parents.php
@@ -52,13 +52,19 @@ class Parents extends SyncNormalizerDecoratorBase implements ContainerFactoryPlu
   public function decorateNormalization(array &$normalized_entity, ContentEntityInterface $entity, $format, array $context = []) {
     if ($entity->hasField('parent')) {
       $entity_type = $entity->getEntityTypeId();
-      if ($entity_type === 'taxonomy_term') {
-        /** @var \Drupal\taxonomy\TermStorageInterface $storage */
-        $storage = $this->entityTypeManager->getStorage($entity_type);
-        $parents = $storage->loadParents($entity->id());
+      $storage = $this->entityTypeManager->getStorage($entity_type);
+      if (method_exists($storage, 'loadParents')) {
+        $parents = $storage->loadParents($entity_id);
         foreach ($parents as $parent_key => $parent) {
-          $normalized_entity['parent'][] = ['target_type' =>'taxonomy_term', 'target_uuid' => $parent->uuid()];
+          $normalized_entity['parent'][] = ['target_type' => $entity_type, 'target_uuid' => $parent->uuid()];
           $normalized_entity['_content_sync']['entity_dependencies'][$entity_type][] =  $entity_type . "." . $parent->bundle() . "." . $parent->uuid();
+        }
+      }elseif (method_exists($entity, 'getParentId')) {
+        $parent = $entity->getParentId();
+        if (($tmp = strstr($parent, ':')) !== false) {
+          $parent_uuid = substr($tmp, 1);
+          $normalized_entity['parent'][] = ['target_type' => $entity_type, 'target_uuid' => $parent_uuid];
+          $normalized_entity['_content_sync']['entity_dependencies'][$entity_type][] =  $entity_type . "." . $entity_type . "." . $parent_uuid;
         }
       }
     }
