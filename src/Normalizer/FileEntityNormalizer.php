5aa98042e4a1ca0a3f18745bed073c8855292fd8
klimp <klimp@1278928.no-reply.drupal.org>
2019-02-28 11:04:09 -0500

    Issue #2980460 by klimp, Blanca.Esqueda, dabito, david4lim: FileEntityNormalizer. ['uri'] should be an array but not a string

--- src/Normalizer/FileEntityNormalizer.php
+++ src/Normalizer/FileEntityNormalizer.php
@@ -64,7 +64,14 @@ class FileEntityNormalizer extends ContentEntityNormalizer {
           if (!file_exists($file) || (md5_file($file) !== md5_file($source))) {
             $dir = $this->fileSystem->dirname($data['uri'][0]['value']);
             file_prepare_directory($dir, FILE_CREATE_DIRECTORY);
-            $data['uri'] = file_unmanaged_copy($source, $data['uri'][0]['value']);
+            $uri = file_unmanaged_copy($source, $data['uri'][0]['value']);
+            $data['uri'] = [
+              [
+                'value' => $uri,
+                'url' => str_replace($GLOBALS['base_url'], '', file_create_url($uri))
+              ]
+            ];
+
             // We just need a method to create the image.
             $file_data = '';
           }
