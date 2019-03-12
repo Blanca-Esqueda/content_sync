78821ef9e85897a2c9713681405b0bbe57030b95
Blanca.Esqueda <Blanca.Esqueda@3082389.no-reply.drupal.org>
2019-03-12 14:04:16 -0400

    Update Import resolver to check for dependencies, if they are not in the active storage pull them from the content sync folder

--- src/DependencyResolver/ImportQueueResolver.php
+++ src/DependencyResolver/ImportQueueResolver.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\content_sync\DependencyResolver;
 
+use Drupal\Core\Serialization\Yaml;
+
 /**
  * Class ImportQueueResolver.
  *
@@ -20,25 +22,50 @@ class ImportQueueResolver implements ContentSyncResolverInterface {
    *   Parsed entities to import.
    */
   protected function depthFirstSearch(array &$visited, array $identifiers, array $normalized_entities) {
+    $validate_dependecies = TRUE;
     foreach ($identifiers as $identifier) {
-
-
-    foreach ($normalized_entities as $identifier => $entity) {
       $ids = explode('.', $identifier);
       list($entity_type_id, $bundle, $uuid) = $ids;
+      // Check if entity was sent on the files to import.
       if (!empty($normalized_entities[$identifier])) {
         $entity = $normalized_entities[$identifier];
+        // Check dependencies
         if (!empty($entity['_content_sync']['entity_dependencies'])) {
           foreach ($entity['_content_sync']['entity_dependencies'] as $ref_entity_type_id => $references) {
-            $this->depthFirstSearch($visited, $references, $normalized_entities);
+            if(!is_null($references)){
+              $dependency = $this->depthFirstSearch($visited, $references, $normalized_entities);
+            if ($dependency !== TRUE && !is_null($dependency)){
+               $validate_dependecies = FALSE;
+               $visited['Missing'][$identifier][] = $dependency;
+            }
+            }
           }
         }
-        if (!isset($visited[$identifier])) {
+        if (!isset($visited[$identifier]) && $validate_dependecies) {
           $visited[$identifier] = [
             'entity_type_id' => $entity_type_id,
             'decoded_entity' => $entity,
           ];
+          return TRUE;
         }
+      }else{
+        //Verify if dependency exist in the site
+        $return = (bool) \Drupal::database()->queryRange('SELECT 1 FROM {cs_db_snapshot} WHERE name = :name', 0, 1, [
+        ':name' => $identifier])->fetchField();
+
+        if (!$return){
+          // Look for the reference in CS folder
+          $file_path = "/Users/Blanca/Sites/devdesktop_opin/do/../content/sync/entities/".$entity_type_id."/".$bundle."/".$identifier.".yml";
+          $entity = file_get_contents($file_path);
+          if($entity){
+            $normalized_entities[$identifier] = Yaml::decode($entity);
+            $this->depthFirstSearch($visited,[$identifier], $normalized_entities);
+            return TRUE;
+          }else{
+            $return = $identifier;
+          }
+        }
+        return $return;
       }
     }
   }
