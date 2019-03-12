753d47caa685d8d25e7b8079f92359a545793c00
klimp <klimp@1278928.no-reply.drupal.org>
2019-03-12 13:59:35 -0400

    Issue #2980457 by klimp: Dependency resolver fix

--- src/DependencyResolver/ImportQueueResolver.php
+++ src/DependencyResolver/ImportQueueResolver.php
@@ -2,58 +2,63 @@
 
 namespace Drupal\content_sync\DependencyResolver;
 
-
-use Drupal\Component\Graph\Graph;
-
+/**
+ * Class ImportQueueResolver.
+ *
+ * @package Drupal\content_sync\DependencyResolver
+ */
 class ImportQueueResolver implements ContentSyncResolverInterface {
 
-  public function resolve(array $normalized_entities) {
-    $queue = [];
-    $graph = [];
-    $uuids = [];
+  /**
+   * Builds a graph placing the deepest vertexes at the first place.
+   *
+   * @param array $visited
+   *   Array of vertexes to return.
+   * @param array $identifiers
+   *   Array of entity identifiers to process.
+   * @param array $normalized_entities
+   *   Parsed entities to import.
+   */
+  protected function depthFirstSearch(array &$visited, array $identifiers, array $normalized_entities) {
+    foreach ($identifiers as $identifier) {
+
+
     foreach ($normalized_entities as $identifier => $entity) {
       $ids = explode('.', $identifier);
       list($entity_type_id, $bundle, $uuid) = $ids;
-      if (!empty($entity['_content_sync']['entity_dependencies'])) {
-        foreach ($entity['_content_sync']['entity_dependencies'] as $ref_entity_type_id => $references) {
-          foreach ($references as $reference) {
-            if (!empty($normalized_entities[$reference])) {
-              $graph[$identifier]['edges'][$reference] = 1;
-            }
+      if (!empty($normalized_entities[$identifier])) {
+        $entity = $normalized_entities[$identifier];
+        if (!empty($entity['_content_sync']['entity_dependencies'])) {
+          foreach ($entity['_content_sync']['entity_dependencies'] as $ref_entity_type_id => $references) {
+            $this->depthFirstSearch($visited, $references, $normalized_entities);
           }
         }
-      }
-      else {
-        $uuids[] = $identifier;
-        $queue[] = [
-          'entity_type_id' => $entity_type_id,
-          'decoded_entity' => $entity,
-        ];
-      }
-
-    }
-    $graph = new Graph($graph);
-    $entities = $graph->searchAndSort();
-    uasort($entities, 'Drupal\Component\Utility\SortArray::sortByWeightElement');
-    foreach ($entities as $uuid => $vertex) {
-      foreach ($vertex['edges'] as $key => $value) {
-        if (!in_array($key, $uuids) && $uuid != $key) {
-          $uuids[] = $key;
-          $ids = explode('.', $key);
-          $queue[] = [
-            'entity_type_id' => $ids[0],
-            'decoded_entity' => $normalized_entities[$key],
+        if (!isset($visited[$identifier])) {
+          $visited[$identifier] = [
+            'entity_type_id' => $entity_type_id,
+            'decoded_entity' => $entity,
           ];
         }
       }
-      $uuids[] = $uuid;
-      $ids = explode('.', $uuid);
-      $queue[] = [
-        'entity_type_id' => $ids[0],
-        'decoded_entity' => $normalized_entities[$uuid],
-      ];
     }
-    return $queue;
+  }
+
+  /**
+   * Creates a queue.
+   *
+   * @param array $normalized_entities
+   *   Parsed entities to import.
+   *
+   * @return array
+   *   Queue to be processed within a batch process.
+   */
+  public function resolve(array $normalized_entities) {
+    $visited = [];
+    foreach ($normalized_entities as $identifier => $entity) {
+      $this->depthFirstSearch($visited, [$identifier], $normalized_entities);
+    }
+    // Reverse the array to adjust it to an array_pop-driven iterator.
+    return array_reverse($visited);
   }
 
 }
