04453626cd75e19d110d2e0e15bfb814fe519d38
Blanca.Esqueda <Blanca.Esqueda@3082389.no-reply.drupal.org>
2019-03-12 13:15:56 -0400

    by Blanca.Esqueda: Expand CS storage database functions to be able to handle collections

--- src/Content/ContentDatabaseStorage.php
+++ src/Content/ContentDatabaseStorage.php
@@ -42,9 +42,45 @@ class ContentDatabaseStorage extends DatabaseStorage {
    */
   protected function cs_doWrite($name, $data, $collection) {
     $options = ['return' => Database::RETURN_AFFECTED] + $this->options;
+    $this->connection->delete($this->table, $options)
+      ->condition('name', $name)
+      ->execute();
+
     return (bool) $this->connection->merge($this->table, $options)
       ->keys(['collection', 'name'], [$collection, $name])
       ->fields(['data' => $data])
+      ->execute();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function cs_read($name) {
+    $data = FALSE;
+    try {
+      $raw = $this->connection->query('SELECT data FROM {' . $this->connection->escapeTable($this->table) . '} WHERE name = :name', [':name' => $name], $this->options)->fetchField();
+      if ($raw !== FALSE) {
+        $data = $this->decode($raw);
+      }
+    }
+    catch (\Exception $e) {
+      // If we attempt a read without actually having the database or the table
+      // available, just return FALSE so the caller can handle it.
+    }
+    return $data;
+  }
+
+  /**
+   * Implements Drupal\Core\Config\StorageInterface::delete().
+   *
+   * @throws PDOException
+   *
+   * @todo Ignore replica targets for data manipulation operations.
+   */
+  public function cs_delete($name) {
+    $options = ['return' => Database::RETURN_AFFECTED] + $this->options;
+    return (bool) $this->connection->delete($this->table, $options)
+      ->condition('name', $name)
       ->execute();
   }
 
