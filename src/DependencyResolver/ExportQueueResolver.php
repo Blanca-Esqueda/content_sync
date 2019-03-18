a9b110766e0a324edc7252499b5f4d4be6280844
Blanca.Esqueda <ing.blanca.esqueda@gmail.com>
2019-03-18 16:45:01 -0400

    DB instead of Array for entity exporr

--- src/DependencyResolver/ExportQueueResolver.php
+++ src/DependencyResolver/ExportQueueResolver.php
@@ -6,28 +6,25 @@ use Drupal\Core\Serialization\Yaml;
 use Drupal\content_sync\Content\ContentDatabaseStorage;
 
 /**
- * Class ImportQueueResolver.
+ * Class ExportQueueResolver.
  *
  * @package Drupal\content_sync\DependencyResolver
  */
 class ExportQueueResolver implements ContentSyncResolverInterface {
 
   /**
-   * Builds a graph placing the deepest vertexes at the first place.
+   * Add dependecies to the quew\ue.
    *
-   * @param array $visited
-   *   Array of vertexes to return.
    * @param array $identifiers
    *   Array of entity identifiers to process.
    * @param array $normalized_entities
    *   Parsed entities to import.
    */
-  protected function depthFirstSearch(array &$visited, array $identifiers, array $normalized_entities) {
-    $validate_dependecies = TRUE;
+  protected function depthFirstSearch( $identifier, array $normalized_entity) {
     foreach ($identifiers as $identifier) {
       $ids = explode('.', $identifier);
       list($entity_type_id, $bundle, $uuid) = $ids;
-      // Check if entity was sent on the files to import.
+      // Check if entity is included in the queue.
       if (!empty($normalized_entities[$identifier])) {
         $entity = $normalized_entities[$identifier];
         // Check dependencies
@@ -36,17 +33,20 @@ class ExportQueueResolver implements ContentSyncResolverInterface {
             if(!is_null($references)){
               $dependency = $this->depthFirstSearch($visited, $references, $normalized_entities);
             
-            if ($dependency !== TRUE && !is_null($dependency)){
-               $validate_dependecies = FALSE;
-               $visited['Missing'][$identifier][] = $dependency;
-            }
+              //if ($dependency !== TRUE && !is_null($dependency)){
+              //   $validate_dependecies = FALSE;
+              //   $visited['Missing'][$identifier][] = $dependency;
+              //}
             }
           }
         }
         if (!isset($visited[$identifier]) && $validate_dependecies) {
-          $visited[$identifier]['entity_type'] = $entity_type_id;
-          $visited[$identifier]['entity_uuid'] = $uuid;
-          return TRUE;
+          //$visited[$identifier]['entity_type'] = $entity_type_id;
+          //$visited[$identifier]['entity_uuid'] = $uuid;
+          //return TRUE;
+
+
+
         }
       }else{
         //Verify if dependency exist in the site and include it.
@@ -63,21 +63,66 @@ class ExportQueueResolver implements ContentSyncResolverInterface {
   }
 
   /**
-   * Creates a queue.
+   * Add dependencies of the entity to export to the queue.
    *
-   * @param array $normalized_entities
-   *   Parsed entities to import.
+   * @param array $normalized_entity
+   *   Parsed entity to export.
    *
-   * @return array
-   *   Queue to be processed within a batch process.
    */
-  public function resolve(array $normalized_entities, $visited = []) {
-    foreach ($normalized_entities as $identifier => $entity) {
-      $this->depthFirstSearch($visited, [$identifier], $normalized_entities);
+  public function resolve(array $normalized_entity) {
+    $identifier = key($normalized_entity);
+    //$this->depthFirstSearch($identifier, $normalized_entity);
+
+
+
+
+
+
+    foreach ($identifiers as $identifier) {
+      $ids = explode('.', $identifier);
+      list($entity_type_id, $bundle, $uuid) = $ids;
+      // Check if entity is included in the queue.
+      if (!empty($normalized_entities[$identifier])) {
+        $entity = $normalized_entities[$identifier];
+        // Check dependencies
+        if (!empty($entity['_content_sync']['entity_dependencies'])) {
+          foreach ($entity['_content_sync']['entity_dependencies'] as $ref_entity_type_id => $references) {
+            if(!is_null($references)){
+              $dependency = $this->depthFirstSearch($visited, $references, $normalized_entities);
+            
+              //if ($dependency !== TRUE && !is_null($dependency)){
+              //   $validate_dependecies = FALSE;
+              //   $visited['Missing'][$identifier][] = $dependency;
+              //}
+            }
+          }
+        }
+        if (!isset($visited[$identifier]) && $validate_dependecies) {
+          //$visited[$identifier]['entity_type'] = $entity_type_id;
+          //$visited[$identifier]['entity_uuid'] = $uuid;
+          //return TRUE;
+
+
+
+        }
+      }else{
+        //Verify if dependency exist in the site and include it.
+        $activeStorage = new ContentDatabaseStorage(\Drupal::database(), 'cs_db_snapshot');
+        $entity = $activeStorage->cs_read($identifier);
+        if($entity){
+          $normalized_entities[$identifier] = $entity;
+          $this->depthFirstSearch($visited,[$identifier], $normalized_entities);
+        }else{
+          $return = $identifier;
+        }
+      }
     }
 
-    // Reverse the array to adjust it to an array_pop-driven iterator.
-    return array_reverse($visited);
-  }
 
+
+
+
+
+
+  }
 }
