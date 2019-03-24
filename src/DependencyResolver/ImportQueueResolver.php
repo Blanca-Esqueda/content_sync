f7b1d0b703bcb9d52fe54463e320932efb65fe10
klimp <klimp.drupal@gmail.com>
2019-03-24 12:35:10 +0100

    Fix by klimp: Fixed dependency resolver

--- src/DependencyResolver/ImportQueueResolver.php
+++ src/DependencyResolver/ImportQueueResolver.php
@@ -22,52 +22,79 @@ class ImportQueueResolver implements ContentSyncResolverInterface {
    *   Parsed entities to import.
    */
   protected function depthFirstSearch(array &$visited, array $identifiers, array $normalized_entities) {
-    $validate_dependecies = TRUE;
     foreach ($identifiers as $identifier) {
-      $ids = explode('.', $identifier);
-      list($entity_type_id, $bundle, $uuid) = $ids;
-      // Check if entity was sent on the files to import.
-      if (!empty($normalized_entities[$identifier])) {
-        $entity = $normalized_entities[$identifier];
-        // Check dependencies
-        if (!empty($entity['_content_sync']['entity_dependencies'])) {
-          foreach ($entity['_content_sync']['entity_dependencies'] as $ref_entity_type_id => $references) {
-            if(!is_null($references)){
-              $dependency = $this->depthFirstSearch($visited, $references, $normalized_entities);
-            if ($dependency !== TRUE && !is_null($dependency)){
-               $validate_dependecies = FALSE;
-               $visited['Missing'][$identifier][] = $dependency;
-            }
-            }
-          }
-        }
-        if (!isset($visited[$identifier]) && $validate_dependecies) {
-          $visited[$identifier] = [
-            'entity_type_id' => $entity_type_id,
-            'decoded_entity' => $entity,
-          ];
-          return TRUE;
-        }
-      }else{
-        //Verify if dependency exist in the site
-        $return = (bool) \Drupal::database()->queryRange('SELECT 1 FROM {cs_db_snapshot} WHERE name = :name', 0, 1, [
-        ':name' => $identifier])->fetchField();
 
-        if (!$return){
-          // Look for the reference in CS folder
-          $file_path = content_sync_get_content_directory(CONFIG_SYNC_DIRECTORY)."/entities/".$entity_type_id."/".$bundle."/".$identifier.".yml";
-          $entity = file_get_contents($file_path);
-          if($entity){
-            $normalized_entities[$identifier] = Yaml::decode($entity);
-            $this->depthFirstSearch($visited,[$identifier], $normalized_entities);
-            return TRUE;
-          }else{
-            $return = $identifier;
-          }
-        }
-        return $return;
+      // Get a decoded entity. FALSE means no need to import.
+      try {
+        $entity = $this->getEntity($identifier, $normalized_entities);
+      } catch (\Exception $e) {
+        $entity = FALSE;
+        $visited['Missing'][$identifier][] = $e->getMessage();
       }
+
+      // Process dependencies first.
+      if (!empty($entity['_content_sync']['entity_dependencies'])) {
+        foreach ($entity['_content_sync']['entity_dependencies'] as $ref_entity_type_id => $references) {
+          $this->depthFirstSearch($visited, $references, $normalized_entities);
+        }
+      }
+
+      if (!isset($visited[$identifier]) && $entity) {
+        list($entity_type_id, $bundle, $uuid) = explode('.', $identifier);
+        $visited[$identifier] = [
+          'entity_type_id' => $entity_type_id,
+          'decoded_entity' => $entity,
+        ];
+      }
+
     }
+  }
+
+  /**
+   * Gets an entity.
+   *
+   * @param $identifier
+   *   An entity identifier to process.
+   * @param $normalized_entities
+   *   An array of entity identifiers to process.
+   *
+   * @return bool|mixed
+   *   Decoded entity or FALSE if an entity already exists and doesn't require to be imported.
+   *
+   * @throws \Exception
+   */
+  protected function getEntity($identifier, $normalized_entities) {
+    if (!empty($normalized_entities[$identifier])) {
+      $entity = $normalized_entities[$identifier];
+    }
+    else {
+
+      // We already have the entity, no need to import.
+      if ($this->entityExists($identifier)) return FALSE;
+
+      list($entity_type_id, $bundle, $uuid) = explode('.', $identifier);
+      $file_path = content_sync_get_content_directory(CONFIG_SYNC_DIRECTORY)."/entities/".$entity_type_id."/".$bundle."/".$identifier.".yml";
+      $raw_entity = file_get_contents($file_path);
+
+      // Problems to open the .yml file.
+      if (!$raw_entity) throw new \Exception("Dependency {$identifier} is missing.");
+
+      $entity = Yaml::decode($raw_entity);
+    }
+    return $entity;
+  }
+
+  /**
+   * Checks if a dependency exists in the site.
+   *
+   * @param $identifier
+   *   An entity identifier to process.
+   *
+   * @return bool
+   */
+  protected function entityExists($identifier) {
+    return (bool) \Drupal::database()->queryRange('SELECT 1 FROM {cs_db_snapshot} WHERE name = :name', 0, 1, [
+      ':name' => $identifier])->fetchField();
   }
 
   /**
